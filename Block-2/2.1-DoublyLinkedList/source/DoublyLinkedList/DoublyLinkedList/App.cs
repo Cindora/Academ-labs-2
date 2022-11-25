76e553a48c0df98bc705f3d93aeb2198c73dc8e1
NikitaSPBU <st111245@student.spbu.ru>
2022-11-25 13:13:44 +0300

    init

---
+++ Block-2/2.1-DoublyLinkedList/source/DoublyLinkedList/DoublyLinkedList/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class App
+    {
+        static void Main()
+        {
+            var list = new DoublyLinkedList<int>();
+        }
+    }
+}
---
+++ Block-2/2.1-DoublyLinkedList/source/DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+        public Item<T> Head { get; set; }
+        public Item<T> Tail { get; set; }
+        public int Count { get; set; }
+
+
+        public DoublyLinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public DoublyLinkedList(T data)
+        {
+            var item = new Item<T>(data);
+            Head = item;
+            Tail = null;
+            Count = 1;
+        }
+    }
+}
