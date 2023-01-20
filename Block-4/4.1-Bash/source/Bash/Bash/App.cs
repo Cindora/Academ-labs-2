b673a303d3f781e039e4362bc8667c9a4a23e6fe
NikitaSPBU <st111245@student.spbu.ru>
2023-01-20 22:08:42 +0300

    init commit

---
+++ Block-4/4.1-Bash/source/Bash/Bash/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bash
+{
+    internal class App
+    {
+        static void Main(string[] args)
+        {
+
+            BashStart Bash = new BashStart();
+            Bash.Start();
+        
+        }
+    }
+}
