9b44d3f2d68b88ca36fe9c449e4b0bc0e335cec4
NikitaSPBU <st111245@student.spbu.ru>
2023-01-30 15:45:09 +0300

    init, add general Request

---
+++ Block-4/4.2-Weather/source/WeatherGraphic/WeatherGraphic/Request.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+using System.Net;
+
+namespace WeatherGraphic
+{
+    public class Request
+    {
+        private HttpWebRequest request;
+        public string Response;
+
+        public void StartRequest(string url)
+        {
+            request = (HttpWebRequest)WebRequest.Create(url);
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    Response = new StreamReader(stream).ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Response = null;
+            }
+        }
+    }
+}
